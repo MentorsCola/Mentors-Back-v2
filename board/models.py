87e791c9b65c0a3391622d558df8a2ac42f26548
shk0625 <tjfgml1238@gmail.com>
2023-11-28 08:54:12 +0900

    📝 board user 제공으로 사용

--- board/models.py
+++ board/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Board(models.Model):  # 제목, 작성자, 내용, 작성일, 마지막 수정일
     title = models.CharField("제목", max_length=50, null=False)
-    author = models.ForeignKey("작성자", on_delete=models.CASCADE)  # User 탈퇴시 게시글 같이 지우기(CASCADE) 쓰는 기능은 아니지만 일단 넣음
+    author = models.ForeignKey(User, verbose_name="작성자", on_delete=models.CASCADE)  # User 탈퇴시 게시글 같이 지우기(CASCADE) 쓰는 기능은 아니지만 일단 넣음
     content = models.TextField("내용", null=False)
     dt_created = models.DateTimeField("작성일", auto_now_add=True, null=False)
     dt_modified = models.DateTimeField("수정일", auto_now_add=True, null=False)
     like = models.IntegerField("하트", default=0)
 
     def __str__(self):
-        return 'author : {}, title : {}'.format(self.author, self.title, self.content)
+        return self.title
+
