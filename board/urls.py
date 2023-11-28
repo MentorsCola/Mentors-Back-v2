b574c5537abcb946dd8ac785b79dc6798c86acb1
shk0625 <tjfgml1238@gmail.com>
2023-11-28 20:51:11 +0900

    ♻️ url pattern change

--- board/urls.py
+++ board/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from . import views
-
-app_name = "board"
+from .views import board_list, board_detail, board_create, board_edit, board_delete
 
 urlpatterns = [
-    path('', views.board_list, name='board_list'),
-    path('board/<int:pk>/', views.board_detail, name='board_detail'),
-    path('board/new/', views.board_create, name='board_create'),
-    path('board/<int:pk>/edit/', views.board_edit, name='board_edit'),
-    path('board/<int:pk>/delete/', views.board_delete, name='board_delete'),
+    path('', board_list, name='board_list'),
+    path('new/', board_create, name='board_create'),
+    path('<int:pk>/', board_detail, name='board_detail'),
+    path('<int:pk>/edit/', board_edit, name='board_edit'),
+    path('<int:pk>/delete/', board_delete, name='board_delete'),
 ]
