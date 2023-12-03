30f5e1023b2e8fb299dadc842f871cd7d9a15d7c
shk0625 <tjfgml1238@gmail.com>
2023-12-03 14:55:07 +0900

    🔥 user 관련 코드 옮김 view

--- board/views.py
+++ board/views.py
@@ -1,6 +1,3 @@
-from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Board
 from board.forms import BoardForm
@@ -14,22 +11,6 @@ def board_list(request):
 def board_detail(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'board/board_detail.html', {'board': board})
-
-
-@login_required
-def board_create(request):
-    if request.method == 'POST':
-        form = BoardForm(request.POST)
-        if form.is_valid():
-            # 현재 로그인한 사용자를 글의 작성자로 지정
-            board = form.save(commit=False)
-            board.author = request.user
-            board.save()
-            return redirect('board_list')
-    else:
-        form = BoardForm()
-
-    return render(request, 'board/board_form.html', {'form': form})
 
 
 def board_edit(request, pk):
@@ -50,16 +31,3 @@ def board_delete(request, pk):
     board = get_object_or_404(Board, pk=pk)
     board.delete()
     return redirect('board_list')
-
-
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('board_list')  # 회원가입 후 이동할 페이지 지정
-    else:
-        form = UserCreationForm()
-
-    return render(request, 'board/signup.html', {'form': form})
