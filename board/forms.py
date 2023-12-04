d6d4400eeb3b9541a6ba498c1fe81824c10b3fe8
shk0625 <tjfgml1238@gmail.com>
2023-12-04 14:14:43 +0900

    ♻️ 폼 변경 (나중에 다 지울거임)

--- board/forms.py
+++ board/forms.py
@@ -1,5 +1,18 @@
 from django import forms
-from board.models import Board
+from django.contrib.auth.forms import UserCreationForm
+from .models import User, Board
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email',)  # 필요한 필드만 포함
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # username과 관련된 필드 제거
+        self.fields.pop('username')
+        self.fields.pop('password2')
 
 
 class BoardForm(forms.ModelForm):
