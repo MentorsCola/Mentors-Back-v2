50db47b6bf743aa4ca2995ea6bfe71d5e1ce9f4b
shk0625 <tjfgml1238@gmail.com>
2023-12-04 14:14:22 +0900

    🎨 generics view 써서 코드 간결하게 하기

--- user/views.py
+++ user/views.py
@@ -1,35 +1,15 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required
-
-from board.forms import BoardForm
-from .forms import CustomUserCreationForm
+from rest_framework import generics, permissions
+from .models import User
+from .serializers import UserSerializer
 
 
-def signup(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')
-    else:
-        form = CustomUserCreationForm()
-
-    return render(request, 'signup.html', {'form': form})
+class UserListView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
-@login_required
-def board_create(request):
-    if request.method == 'POST':
-        form = BoardForm(request.POST)
-        if form.is_valid():
-            # 현재 로그인한 사용자를 글의 작성자로 지정
-            board = form.save(commit=False)
-            board.author = request.user
-            board.save()
-            return redirect('board_list')
-    else:
-        form = BoardForm()
-
-    return render(request, 'board/board_form.html', {'form': form})
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
