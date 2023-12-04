6c8d2b6f2457518a50a340437ccbc2864430523c
shk0625 <tjfgml1238@gmail.com>
2023-12-04 15:04:35 +0900

    🔥 response 만들기위해 테스트 폼 삭제 된 엔드포인트 삭제

--- board/urls.py
+++ board/urls.py
@@ -1,15 +1,12 @@
-from django.contrib.auth.views import LoginView
 from django.urls import path
-
-from user.views import board_create, signup
 from .views import board_list, board_detail, board_edit, board_delete
 
 urlpatterns = [
     path('', board_list, name='board_list'),
-    path('new/', board_create, name='board_create'),
+
     path('<int:pk>/', board_detail, name='board_detail'),
     path('<int:pk>/edit/', board_edit, name='board_edit'),
     path('<int:pk>/delete/', board_delete, name='board_delete'),
-    path('signup/', signup, name='signup'),
-    path('login/', LoginView.as_view(), name='login'),
+
+
 ]
