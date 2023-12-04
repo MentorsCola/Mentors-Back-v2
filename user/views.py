6cd7f1fddb1b9fa2c7b9ffe92111ebda74c2a2db
shk0625 <tjfgml1238@gmail.com>
2023-12-04 21:14:45 +0900

    ♻️ 회원가입 누구나 가능하게 인가 수정

--- user/views.py
+++ user/views.py
@@ -6,7 +6,7 @@ from .serializers import UserSerializer
 class UserListView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
 
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
