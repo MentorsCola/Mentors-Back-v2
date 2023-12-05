ca3e9b9e8caed8ae037649eb66d1b9fe069d80d2
shk0625 <tjfgml1238@gmail.com>
2023-12-05 12:14:14 +0900

    💡 디테일 뷰 나중에 커스텀

--- user/views.py
+++ user/views.py
@@ -9,7 +9,7 @@ class UserListView(generics.ListCreateAPIView):
     permission_classes = [permissions.AllowAny]
 
 
-class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView): # 회원정보 수정 전용인데 이거 나중에 커스텀 할거임. 우리 프로젝엔 필요없
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
