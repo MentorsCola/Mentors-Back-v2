81a13a5a54a29ec21c769d9251e1b6e323bc25ff
shk0625 <tjfgml1238@gmail.com>
2023-12-06 21:15:29 +0900

    ✨ return add

--- like/models.py
+++ like/models.py
@@ -5,5 +5,8 @@ from user.models import User
 
 
 class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE)
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user} likes {self.board}'
