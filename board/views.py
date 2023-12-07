40ca5f12415b13c3a084f257a2ad331c6b5c20e4
shk0625 <tjfgml1238@gmail.com>
2023-12-07 15:17:38 +0900

    🎨 cbv로 변경 및 필드 참조 수정

--- board/views.py
+++ board/views.py
@@ -1,13 +1,31 @@
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
+from rest_framework import permissions
+from rest_framework.views import APIView
+
 from .models import Board
 from board.forms import BoardForm
 
 
-def board_list(request):
-    boards = Board.objects.all()
-    return render(request, 'board/board_list.html', {'boards': boards})
+class BoardList(APIView):
+    permission_classes = [permissions.AllowAny]
+    def get(self, request):
+        boards = Board.objects.all()
+        board_data = []
+
+        for board in boards:
+            board_data.append({
+                'id': board.id,
+                'title': board.title,
+                'nickname_author': board.nickname_author.nicknames if board.nickname_author else None,
+                'dt_created': board.dt_created,
+                'dt_modified': board.dt_modified
+            })
+
+        return JsonResponse({'boards': board_data})
 
 
+class Board_detail(View):
 def board_detail(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'board/board_detail.html', {'board': board})
