a45354da12ae9b3fc3a8808380e0ce1a43614477
shk0625 <tjfgml1238@gmail.com>
2023-12-08 01:25:03 +0900

    ✨ 테이블 이름 명시적으로 설정

--- like/models.py
+++ like/models.py
@@ -8,6 +8,8 @@ class Like(models.Model):
     user_email = models.ForeignKey('user.User', on_delete=models.CASCADE)
     board_id = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='liked_users')
 
+    def __str__(self):
+        return f'{self.user} likes {self.board}'
 
-def __str__(self):
-    return f'{self.user} likes {self.board}'
+    class Meta:
+        db_table = 'like'
