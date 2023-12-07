28d45857418f2052202a9472e732dbfbfcb7ac86
shk0625 <tjfgml1238@gmail.com>
2023-12-07 10:20:18 +0900

    🐛 역참조 실수

--- like/models.py
+++ like/models.py
@@ -6,7 +6,8 @@ from user.models import User
 
 class Like(models.Model):
     user_email = models.ForeignKey('user.User', on_delete=models.CASCADE)
-    board_id = models.ForeignKey(Board, on_delete=models.CASCADE)
+    board_id = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='liked_users')
 
-    def __str__(self):
+
+def __str__(self):
         return f'{self.user} likes {self.board}'
