7dc6173f174f1757c3cf8bee38576e254f36cdd5
shk0625 <tjfgml1238@gmail.com>
2023-12-08 15:48:58 +0900

    🐛 import 수정... ㅠ

--- board/models.py
+++ board/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-from MentorsBackV2 import settings
-from user.models import Nickname
+
+import user
+from user.models import Nickname, User
 
 
 class Board(models.Model):  # 제목, 작성자, 내용, 작성일, 마지막 수정일
     title = models.CharField("제목", max_length=50, null=False)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField("내용", null=False)
     dt_created = models.DateTimeField("작성일", auto_now_add=True, null=False)
     dt_modified = models.DateTimeField("수정일", auto_now=True, null=False)
