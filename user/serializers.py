5b13df222e599e4e31134d281f0ee556b0c75b38
shk0625 <tjfgml1238@gmail.com>
2023-12-10 06:59:10 +0900

    🐛 회원가입 필드 저장 안 되는 거 오류 수정

--- user/serializers.py
+++ user/serializers.py
@@ -6,4 +6,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'password', 'id_nickname'] # id_nickname fk
-        read_only_fields = ['id', 'email', 'password', 'id_nickname']
+        read_only_fields = ['id','email', 'password', 'id_nickname']
