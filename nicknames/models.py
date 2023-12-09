e20c5c8c05b3cce44d831d103234f6c934cc0b1e
shk0625 <tjfgml1238@gmail.com>
2023-12-10 07:37:44 +0900

    ♻️ 공백, 널 불가능

--- nicknames/models.py
+++ nicknames/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Nicknames(models.Model):
     id = models.IntegerField(primary_key=True)
-    names = models.CharField(max_length=100, blank=True, null=True)
+    names = models.CharField(max_length=100, blank=False, null=False)
 
     class Meta:
         managed = False
