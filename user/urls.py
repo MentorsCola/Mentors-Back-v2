8dc341bb8c0496f2bec7edd5a88865d63a0b4b1f
shk0625 <tjfgml1238@gmail.com>
2023-12-11 23:37:53 +0900

    url patterns edit

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from user.views import UserListView, UserDetailView, LoginAPIView
+from user.views import UserListView, UserDetailView, LoginAPIView, UserAPIView
 
 urlpatterns = [
+    path('register/', UserAPIView.as_view(), name='user-register'),
     path('users/', UserListView.as_view(), name='user-list, user-signup'),
     path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
     path('login/', LoginAPIView.as_view(), name='user-login'),
