1b7cb629612651393b8ba732360d5e1065a7cc3d
shk0625 <tjfgml1238@gmail.com>
2023-12-11 12:19:07 +0900

    ✨ 새로운 url

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from user.views import UserListView, UserDetailView
+from user.views import UserListView, UserDetailView, LoginAPIView
 
 urlpatterns = [
-    path('users/', UserListView.as_view(), name='user-list'),
+    path('users/', UserListView.as_view(), name='user-list, user-signup'),
     path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('login/', LoginAPIView.as_view(), name='user-login'),
 ]
