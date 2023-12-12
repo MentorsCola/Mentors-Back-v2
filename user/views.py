2aed927d9967e3b0d680e8818a24649672cd297b
shk0625 <tjfgml1238@gmail.com>
2023-12-12 19:46:21 +0900

    csrf 허용 도메인설정 (x)

--- user/views.py
+++ user/views.py
@@ -14,7 +14,7 @@ class UserListView(generics.ListCreateAPIView):
     permission_classes = [permissions.AllowAny]
 
 
-class UserDetailView(generics.RetrieveUpdateDestroyAPIView): # 회원정보 수정 전용인데 이거 나중에 커스텀 할거임. 우리 프로젝엔 필요없
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):  # 회원정보 수정 전용인데 이거 나중에 커스텀 할거임. 우리 프로젝엔 필요없
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -36,6 +36,9 @@ class UserAPIView(APIView):
             }, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+CSRF_TRUSTED_ORIGINS = ["http://http://127.0.0.1:8000/user/login/"]
 
 
 class LoginAPIView(APIView):
