5d4218e35c090822a246ee29769c11b114788360
shk0625 <tjfgml1238@gmail.com>
2023-12-13 20:02:14 +0900

    view 재정비

--- user/views.py
+++ user/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth import authenticate
-from rest_framework import generics, permissions, status
-from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
+from django.contrib.auth import get_user_model
 from rest_framework.views import APIView
-
-from .models import User
+from rest_framework.response import Response
+from rest_framework import status, generics, permissions
+from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserSerializer
+
+User = get_user_model()
 
 
 class UserListView(generics.ListCreateAPIView):
@@ -14,48 +14,28 @@ class UserListView(generics.ListCreateAPIView):
     permission_classes = [permissions.AllowAny]
 
 
-class UserDetailView(generics.RetrieveUpdateDestroyAPIView):  # 회원정보 수정 전용인데 이거 나중에 커스텀 할거임. 우리 프로젝엔 필요없
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class UserAPIView(APIView):
-    def post(self, request, *args, **kwargs):
+class RegisterAPIView(APIView):
+    def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
 
-            # Generate a token for the newly registered user
-            token, created = Token.objects.get_or_create(user=user)
-
-            return Response({
-                'user_id': user.id,
-                'email': user.email,
-                'token': token.key,
-            }, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'email': user.email, 'token': access_token}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginAPIView(APIView):
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         email = request.data.get('email')
         password = request.data.get('password')
 
-        # Authenticate the user
-        user = authenticate(request, email=email, password=password)
+        user = User.objects.filter(email=email).first()
 
-        if user is not None:
-            # Create or retrieve an authentication token for the user
-            token, created = Token.objects.get_or_create(user=user)
+        if user and user.check_password(password):
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
 
-            # Return the user data along with the token
-            return Response({
-                'user_id': user.id,
-                'email': user.email,
-                'token': token.key,
-            }, status=status.HTTP_200_OK)
-        else:
-            return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-
+            return Response({'user_id': user.id, 'email': user.email, 'token': access_token}, status=status.HTTP_200_OK)
+        return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
