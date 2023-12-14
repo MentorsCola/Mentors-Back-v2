6fc6123451a064f341dfbd3143c69586390f3af3
shk0625 <tjfgml1238@gmail.com>
2023-12-14 19:51:10 +0900

    🐛 올바르게 경로 수정

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from user.views import UserListView, LoginAPIView, RegisterAPIView
+from user.views import UserListView, LoginAPIView, RegisterAPIView, Logout
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='user-register'),
     path('users/', UserListView.as_view(), name='user-list, user-signup'),
     path('login/', LoginAPIView.as_view(), name='user-login'),
+    path('logout/', Logout.as_view(), name='user-logout'),
+    path("auth/refresh/", TokenRefreshView.as_view()), # jwt 토큰 재발급
 ]
