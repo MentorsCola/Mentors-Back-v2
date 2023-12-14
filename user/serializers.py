84b988c0b3073c288582400cb6358264c3576a33
shk0625 <tjfgml1238@gmail.com>
2023-12-14 19:51:01 +0900

    🐛 jwt token 시리얼라이저

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,3 @@
-from rest_framework_simplejwt.tokens import RefreshToken
-
 from .models import User
 from rest_framework import serializers
 
@@ -7,22 +5,11 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'password', 'id_nickname']
+        fields = '__all__'
 
     def create(self, validated_data):
-        # User 객체 생성
         user = User.objects.create_user(
-            email=validated_data['email'],
-            password=validated_data['password'],
+            email = validated_data['email'],
+            password = validated_data['password']
         )
-
-        # 토큰 발행
-        refresh = RefreshToken.for_user(user)
-        access_token = str(refresh.access_token)
-
-        # 항상 id_nickname 값이 있는 경우 반환
-        return {
-            'email': user.email,
-            'id_nickname': user.id_nickname,
-            'token': access_token,
-        }
+        return user
