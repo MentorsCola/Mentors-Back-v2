c85ad8f187643cb4a6d57757b59fcc3ec2fc67b3
shk0625 <tjfgml1238@gmail.com>
2023-12-14 18:02:05 +0900

    bug :: user 객체 반환

--- user/views.py
+++ user/views.py
@@ -1,8 +1,11 @@
-from django.contrib.auth import get_user_model
-from rest_framework.views import APIView
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework import generics, permissions, status
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from rest_framework import status, generics, permissions
+from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
+
+from .models import User
 from .serializers import UserSerializer
 
 User = get_user_model()
@@ -16,6 +19,20 @@ class UserListView(generics.ListCreateAPIView):
 
 class RegisterAPIView(APIView):
     def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+
+            # 토큰 발행 (리프레시 토큰 사용 안 함)
+            access_token = RefreshToken.for_user(user).access_token
+            access_token = str(access_token)
+
+            return Response({'email': user.email, 'token': access_token}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserAPIView(APIView):
+    def post(self, request, *args, **kwargs):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
